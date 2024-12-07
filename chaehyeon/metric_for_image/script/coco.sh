992451717198d74142e2bca358a25db332d2385d
kchaehyeon01 <kchaehyeon01@gmail.com>
2024-12-08 00:48:09 +0900

    kch 241208 : metric_for_image edit

---
+++ chaehyeon/metric_for_image/script/coco.sh
@@ -0,0 +1,7 @@
+# 실행 : /home/chaehyeon/code_answer_with_images/metric_for_image
+cd /home/chaehyeon/code_answer_with_images/metric_for_image/data_coco
+# wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip &
+# wget http://images.cocodataset.org/zips/val2017.zip
+
+unzip annotations_trainval2017.zip
+unzip val2017.zip
